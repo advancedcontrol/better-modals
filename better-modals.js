625a0ef2a4f448fa3cc443845fc18178415ccf20
Stephen von Takach <steve@cotag.me>
2015-09-18 08:34:11 +1000

    init commit

---
+++ better-modals.js
@@ -0,0 +1,93 @@
+(function (angular) {
+    'use strict';
+
+
+    // Mix in to our module
+    var module;
+    try {
+        module = angular.module('coUtils');
+    } catch (e) {
+        module = angular.module('coUtils', []);
+    }
+
+
+    // Globals
+    var container = document.createElement('div');
+
+    // This is where our popup elements will reside
+    container = angular.element(container);
+    container.attr('id', 'modals');
+    angular.element(document.body).append(container);
+
+
+    module
+        .directive('modal', ['$timeout', '$animate', function($timeout, $animate) {
+            return {
+                scope: {
+                    title: '@',
+                    show: '=',
+                    visible: '=?'
+                },
+                restrict: 'E',
+                transclude: true,
+                template:
+                    '<div ng-cloak ng-click="close($event)" ng-touch="close($event)">' +
+                        '<div ng-touch="$event.stopPropagation()">' +
+                            '<div class="modal-header"><h1>{{title}}</h1>' +
+                                '<span ng-if="showClose" class="close" ng-click="close($event)" ng-touch="close($event)"></span>' +
+                            '</div>' +
+                            '<div class="modal-content" ng-transclude></div>' +
+                        '</div>' +
+                    '</div>',
+                link: function(scope, element, attrs) {
+                    var show = function () {
+                            container.append(element);
+                            scope.show = scope.visible = true;
+                            return $animate.addClass(element, 'coModal');
+                        },
+                        close = function () {
+                            return $animate.removeClass(element, 'coModal').then(function () {
+                                element.detach();
+                                scope.show = scope.visible = false;
+                            });
+                        };
+
+
+                    // Setup the element
+                    scope.visible = false;
+                    element.addClass(attrs.animation || 'bounceInRight');
+                    scope.showClose = !attrs.hasOwnProperty('noClose');
+
+                    // Watch to see if the modal should be showing or not
+                    scope.$watch('show', function (showing) {
+                        if (showing && !scope.visible) {
+                            show();
+                        } else if (!showing && scope.visible) {
+                            close();
+                        }
+                    });
+                    
+                    // So the element can close itself
+                    scope.close = function (e) {
+                        if (scope.showClose) {
+                            close();
+                            e.stopPropagation();
+                            e.preventDefault();
+                        }
+                    };
+
+                    // Detach the element from the DOM
+                    element.detach();
+
+                    // On destroy, unregister the popup
+                    scope.$on('$destroy', function () {
+                        close()['finally'](function () {
+                            element.remove();
+                            element = null;
+                        });
+                    });
+                }
+            };
+        }]);
+
+}(this.angular));
